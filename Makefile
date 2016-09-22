e5b6bd53c1d4516e0b224195b7b4bb91c140158d
Francis Deslauriers <francis.deslauriers@mail.utoronto.ca>
2016-09-22 12:44:26 -0400

    Add multithread test cases

--- Makefile
+++ Makefile
@@ -1,13 +1,20 @@
 CC=gcc
-CFLAGS=-O3 -Wall
-DEFINES=-DNUM_ITER=10000000
+CFLAGS=-O3
+LIBS=-pthread
+DEFINES=-DNUM_ITER=1000000
 
-all: failing-open failing-close
+all: failing-open-st failing-open-mt failing-close-st failing-close-mt
 
-failing-open: main.c
-	$(CC) $(CFLAGS) $(DEFINES) -DFAILING_OPEN -o $@ $^
+failing-open-st: main.c
+	$(CC) $(CFLAGS) $(DEFINES) -DFAILING_OPEN_ST -o $@ $^
 
-failing-close: main.c
-	$(CC) $(CFLAGS) $(DEFINES) -DFAILING_CLOSE -o $@ $^
+failing-close-st: main.c
+	$(CC) $(CFLAGS) $(DEFINES) -DFAILING_CLOSE_ST -o $@ $^
+
+failing-open-mt: main.c
+	$(CC) $(CFLAGS) $(LIBS) $(DEFINES) -DFAILING_OPEN_MT -o $@ $^
+
+failing-close-mt: main.c
+	$(CC) $(CFLAGS) $(LIBS) $(DEFINES) -DFAILING_CLOSE_MT -o $@ $^
 clean:
-	rm -f a.out failing-*
+	rm -f failing-*
--- main.c
+++ main.c
@@ -1,17 +1,23 @@
-#include <sys/types.h>
-#include <sys/stat.h>
 #include <fcntl.h>
-#include <sys/time.h>
-#include <unistd.h>
+#include <pthread.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 #ifndef NUM_ITER
 #define NUM_ITER 10000000
 #endif
 
+#ifndef NUM_THREAD
+#define NUM_THREAD 16
+#endif
+
 #define MISSING_FILE "/var/foo"
 
-void failing_open()
+static void* failing_open()
 {
 	int fd, i;
 
@@ -20,14 +26,44 @@ void failing_open()
 		fd = open(MISSING_FILE, O_RDONLY);
 	}
 }
-void failing_close()
+static void* failing_close(void *a)
 {
 	int i;
-	int fd = 4242;
+	int fd = ((long)a)+4242;
 
 	for(i = 0; i < NUM_ITER; i++) {
 		/* will fail with EBADF since the fd is invalid */
 		close(fd);
+	}
+}
+
+static void failing_open_mt()
+{
+	int i, err;
+	void *tret;
+	pthread_t *tids;
+	tids = calloc(NUM_THREAD, sizeof(*tids));
+	for (i = 0; i < NUM_THREAD; i++) {
+		err = pthread_create(&tids[i], NULL, failing_open, NULL);
+	}
+
+	for (i = 0; i < NUM_THREAD; i++) {
+		err = pthread_join(tids[i], &tret);
+	}
+}
+
+static void failing_close_mt()
+{
+	int i, err;
+	void *tret;
+	pthread_t *tids;
+	tids = calloc(NUM_THREAD, sizeof(*tids));
+	for (i = 0; i < NUM_THREAD; i++) {
+		err = pthread_create(&tids[i], NULL, failing_close, (void *) (long) i);
+	}
+
+	for (i = 0; i < NUM_THREAD; i++) {
+		err = pthread_join(tids[i], &tret);
 	}
 }
 int main()
@@ -35,11 +71,19 @@ int main()
 	struct timeval tval_before, tval_after, tval_result;
 	gettimeofday(&tval_before, NULL);
 
-#ifdef FAILING_OPEN
+#ifdef FAILING_OPEN_ST
 	failing_open();
 #endif
-#ifdef FAILING_CLOSE
-	failing_close();
+
+#ifdef FAILING_CLOSE_ST
+	failing_close(0);
+#endif
+
+#ifdef FAILING_OPEN_MT
+	failing_open_mt();
+#endif
+#ifdef FAILING_CLOSE_MT
+	failing_close_mt();
 #endif
 	gettimeofday(&tval_after, NULL);
 	timersub(&tval_after, &tval_before, &tval_result);
