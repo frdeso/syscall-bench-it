06bc8e04decb15c901727b8d546f660ae86b2d5c
Francis Deslauriers <francis.deslauriers@efficios.com>
2016-09-23 18:56:24 -0400

    Reuse same code for single and multi thread testcases

--- Makefile
+++ Makefile
@@ -2,18 +2,13 @@ CC=gcc
 CFLAGS=-O3
 LIBS=-pthread
 
-all: failing-open-st failing-open-mt failing-close-st failing-close-mt
+all: failing-open failing-close
 
-failing-open-st: main.c
-	$(CC) $(CFLAGS) -DFAILING_OPEN_ST -o $@ $^
 
-failing-close-st: main.c
-	$(CC) $(CFLAGS) -DFAILING_CLOSE_ST -o $@ $^
+failing-open: main.c
+	$(CC) $(CFLAGS) $(LIBS) -DFAILING_OPEN -o $@ $^
 
-failing-open-mt: main.c
-	$(CC) $(CFLAGS) $(LIBS) -DFAILING_OPEN_MT -o $@ $^
-
-failing-close-mt: main.c
-	$(CC) $(CFLAGS) $(LIBS) -DFAILING_CLOSE_MT -o $@ $^
+failing-close: main.c
+	$(CC) $(CFLAGS) $(LIBS) -DFAILING_CLOSE -o $@ $^
 clean:
 	rm -f failing-*
--- main.c
+++ main.c
@@ -8,36 +8,36 @@
 #include <unistd.h>
 
 
-#define MISSING_FILE "/var/foo"
 
 long num_iter;
 int num_threads;
 
-static void* failing_open()
+static void *failing_open()
 {
 	int fd, i;
 
-	for(i = 0; i < num_iter; i++) {
+	for (i = 0; i < num_iter; i++) {
 		/* Will fail with ENOENT since the file does not exist */
-		fd = open(MISSING_FILE, O_RDONLY);
+		fd = open(NULL, O_RDONLY);
 	}
 }
-static void* failing_close(void *a)
+static void *failing_close(void *a)
 {
 	int i;
-	int fd = ((long)a)+4242;
+	int fd = -1;
 
-	for(i = 0; i < num_iter; i++) {
+	for (i = 0; i < num_iter; i++) {
 		/* will fail with EBADF since the fd is invalid */
 		close(fd);
 	}
 }
 
-static void failing_open_mt()
+static void failing_open_mt(void)
 {
 	int i, err;
 	void *tret;
 	pthread_t *tids;
+
 	tids = calloc(num_threads, sizeof(*tids));
 	for (i = 0; i < num_threads; i++) {
 		err = pthread_create(&tids[i], NULL, failing_open, NULL);
@@ -48,11 +48,12 @@ static void failing_open_mt()
 	}
 }
 
-static void failing_close_mt()
+static void failing_close_mt(void)
 {
 	int i, err;
 	void *tret;
 	pthread_t *tids;
+
 	tids = calloc(num_threads, sizeof(*tids));
 	for (i = 0; i < num_threads; i++) {
 		err = pthread_create(&tids[i], NULL, failing_close, (void *) (long) i);
@@ -65,30 +66,24 @@ static void failing_close_mt()
 
 int main(int argc, char *argv[])
 {
+	long num_event, time_diff;
+	struct timeval tval_before, tval_after, tval_result;
+
 	num_threads = strtol(argv[1], NULL, 10);
 	num_iter = strtol(argv[2], NULL, 10);
 
-	long num_event;
-	struct timeval tval_before, tval_after, tval_result;
 	gettimeofday(&tval_before, NULL);
 
-#ifdef FAILING_OPEN_ST
-	failing_open();
-#endif
-
-#ifdef FAILING_CLOSE_ST
-	failing_close(0);
-#endif
-
-#ifdef FAILING_OPEN_MT
+#ifdef FAILING_OPEN
 	failing_open_mt();
 #endif
-#ifdef FAILING_CLOSE_MT
+#ifdef FAILING_CLOSE
 	failing_close_mt();
 #endif
 	gettimeofday(&tval_after, NULL);
 	timersub(&tval_after, &tval_before, &tval_result);
-	long time_diff = (tval_result.tv_sec*1000000) + tval_result.tv_usec;
+	time_diff = (tval_result.tv_sec * 1000000) + tval_result.tv_usec;
+
 	printf("%ld", time_diff);
 	return 0;
 }
