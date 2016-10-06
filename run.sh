b7ac6ae25cb8118e38b8f7ffa3eefc53ad99aea6
Francis Deslauriers <francis.deslauriers@efficios.com>
2016-10-06 10:34:26 -0400

    Add memory usage tracker

--- run.sh
+++ run.sh
@@ -4,11 +4,14 @@ cleanup() {
 }
 trap cleanup SIGTERM SIGINT
 
-sleep_time=500000000
+sleep_time=5000000000
 
 duration=0
 tot_nb_iter=0
 nb_events=0
+max_mem=0
+
+MEM_USAGE_FILE=./results/mem-tmp.csv
 
 run_baseline() {
 	testcase=$1
@@ -83,17 +86,38 @@ run_sysdig() {
 	rm $tmp
 }
 
+drop_caches() {
+	sudo sh -c 'echo 3 > /proc/sys/vm/drop_caches'
+}
+start_mem_tracker() {
+	cp /dev/null $MEM_USAGE_FILE
+	nohup watch -n 1 "free | grep 'Mem'| awk '{print \$6}' >> $MEM_USAGE_FILE" &
+}
+
+update_max_mem_usage() {
+	kill -2 $(pidof watch)
+	sleep 1
+	max_mem=0 
+	for i in $(cat $MEM_USAGE_FILE); do
+		(($i > $max_mem)) && max_mem=$i
+	done
+}
+
 
 file_output=./results/results-$sleep_time-8.csv
-echo 'testcase,tracer,run,sleeptime,cpu_affinity,nbthreads,duration,nbiter,nbevents' > $file_output
+echo 'testcase,tracer,run,sleeptime,cpu_affinity,nbthreads,duration,nbiter,nbevents,maxmem' > $file_output
 for nthreads in 1; do
-	for cpuaffinity in 0 1; do
+	for cpuaffinity in 0; do
 		for tcase in failing-open-enoent; do
-			for tracer in  lttng sysdig; do
+			for tracer in baseline lttng sysdig; do
 				for i in $(seq 1 3); do
+					drop_caches
+					start_mem_tracker
+					
 					run_$tracer $tcase $cpuaffinity $nthreads $sleep_time
-					echo $tcase,$tracer,$i,$sleep_time,$cpuaffinity,$nthreads,$duration,$tot_nb_iter,$nb_events >> $file_output
-					echo $tcase,$tracer,$i,$sleep_time,$cpuaffinity,$nthreads,$duration,$tot_nb_iter,$nb_events
+					update_max_mem_usage
+					echo $tcase,$tracer,$i,$sleep_time,$cpuaffinity,$nthreads,$duration,$tot_nb_iter,$nb_events,$max_mem >> $file_output
+					echo $tcase,$tracer,$i,$sleep_time,$cpuaffinity,$nthreads,$duration,$tot_nb_iter,$nb_events,$max_mem
 					sleep 1
 				done
 			done
