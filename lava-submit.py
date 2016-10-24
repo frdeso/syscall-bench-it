8a7e462b9487c0ef184b85c92d666b7eb4dc669a
Francis Deslauriers <francis.deslauriers@efficios.com>
2016-10-24 14:25:27 -0400

    Print lava job end status

--- lava-submit.py
+++ lava-submit.py
@@ -125,3 +125,5 @@ jobstatus = server.scheduler.job_status(jobid)
 while jobstatus in 'Submitted' or jobstatus in 'Running':
     sleep(30)
     jobstatus = server.scheduler.job_status(jobid)
+
+print(jobstatus)
