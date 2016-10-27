8c086ba65d83b9d886b99bb3faf48c1242056a9b
Francis Deslauriers <francis.deslauriers@efficios.com>
2016-10-27 12:15:45 -0400

    Fix job dict before initialization.

--- lava-submit.py
+++ lava-submit.py
@@ -116,8 +116,8 @@ setup_action = {
                 ]
             }
         }
-job_dict['job_name']=job_name
 job_dict= json.loads(job)
+job_dict['job_name']=job_name
 job_dict['actions'].insert(0, deploy_action)
 job_dict['actions'].insert(4, setup_action)
 
