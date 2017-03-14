e5fe9b25a30f04e0092fef9c74f8e3576d711894
Francis Deslauriers <francis.deslauriers@efficios.com>
2017-03-14 16:04:08 -0400

    Enable only events tested by the current test case
    Signed-off-by: Francis Deslauriers <francis.deslauriers@efficios.com>

--- run.sh
+++ run.sh
@@ -31,8 +31,10 @@ run_lttng() {
 	cpu_affinity=$2
 	nbThreads=$3
 	sleepTime=$4
+	traced_events=$5
 	discard_events=0
 	LOCK=1
+
 
 	trap 'LOCK=0' SIGUSR1
 
@@ -44,9 +46,22 @@ run_lttng() {
 
 	lttng create --snapshot --output="$(mktemp -d --tmpdir=/tmp/)"
 	lttng enable-channel --num-subbuf 512 --subbuf-size 64k --kernel my_channel
-	lttng enable-event -k sched_process_exit,sched_switch,signal_deliver --channel my_channel
-	lttng enable-event -k --syscall --all --channel my_channel
+
+        #iterate over the comma-separated list of event
+        for event in ${traced_events//,/ }
+        do
+          if [[ $event == sys_* ]] ;
+          then
+            # event = sys_close ===> sys_event = close
+            sys_event=$(echo $event| cut -d'_' -f 2)
+	    lttng enable-event -k --syscall $sys_event --channel my_channel
+	  else
+	    lttng enable-event -k $event --channel my_channel
+	  fi
+	done
+
 	lttng start
+
 	# we run the testcase twice to ensure the tracer's and testcase's pages are warm
 	output=$(taskset -c 0 ./$testcase $cpu_affinity $nbThreads $sleepTime)
 
@@ -72,9 +87,11 @@ drop_caches() {
 
 file_output=./results.csv
 if [ -z ${1+x} ]; then
-	testcase_to_run="failing-open-enoent"
+	testcase_to_run="failing-open-efault"
+	traced_events="sys_open"
 else
 	testcase_to_run=$1
+        traced_events=$2
 fi
 
 echo 'testcase,tracer,run,sleeptime,cpu_affinity,nbthreads,duration,nbiter,nbevents,discarded,maxmem' > $file_output
@@ -85,7 +102,7 @@ for nthreads in 1 2 4 8 16; do
 			        # Run the testcase once to warm the caches
 			        # and discard the results
 				for i in $(seq 1 5); do
-					run_$tracer $tcase $cpuaffinity $nthreads $sleep_time
+					run_$tracer $tcase $cpuaffinity $nthreads $sleep_time $traced_events
 					echo "$tcase,$tracer,$i,$sleep_time,$cpuaffinity,$nthreads,$duration,$tot_nb_iter,$nb_events,$discard_events,$max_mem" >> $file_output
 					echo "$tcase,$tracer,$i,$sleep_time,$cpuaffinity,$nthreads,$duration,$tot_nb_iter,$nb_events,$discard_events,$max_mem"
 					sleep 1
